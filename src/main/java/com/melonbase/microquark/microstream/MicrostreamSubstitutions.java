bc8db3f69c3267c33a8f8fd0ac8ade936442e9d8
Bernhard Lutzmann <bernhard.lutzmann@gmail.com>
2021-02-05 21:59:31 +0100

    Merge substitutions

--- src/main/java/com/melonbase/microquark/microstream/MicrostreamSubstitutions.java
+++ src/main/java/com/melonbase/microquark/microstream/MicrostreamSubstitutions.java
@@ -4,9 +4,11 @@ import com.oracle.svm.core.annotate.Alias;
 import com.oracle.svm.core.annotate.RecomputeFieldValue;
 import com.oracle.svm.core.annotate.RecomputeFieldValue.Kind;
 import com.oracle.svm.core.annotate.TargetClass;
+import one.microstream.memory.sun.JdkInternals;
 
-@TargetClass(className = "one.microstream.memory.sun.JdkInternals")
-final class Target_one_microstream_memory_sun_JdkInternals0 {
+@TargetClass(JdkInternals.class)
+final class Target_one_microstream_memory_sun_JdkInternals {
+
   @Alias
   @RecomputeFieldValue(
       kind = Kind.FieldOffset,
@@ -14,10 +16,7 @@ final class Target_one_microstream_memory_sun_JdkInternals0 {
       name = "address"
   )
   static long FIELD_OFFSET_Buffer_address;
-}
 
-@TargetClass(className = "one.microstream.memory.sun.JdkInternals")
-final class Target_one_microstream_memory_sun_JdkInternals1 {
   @Alias
   @RecomputeFieldValue(
       kind = Kind.FieldOffset,
@@ -25,10 +24,7 @@ final class Target_one_microstream_memory_sun_JdkInternals1 {
       name = "cleaner"
   )
   static long FIELD_OFFSET_DirectByteBuffer_cleaner;
-}
 
-@TargetClass(className = "one.microstream.memory.sun.JdkInternals")
-final class Target_one_microstream_memory_sun_JdkInternals2 {
   @Alias
   @RecomputeFieldValue(
       kind = Kind.FieldOffset,
