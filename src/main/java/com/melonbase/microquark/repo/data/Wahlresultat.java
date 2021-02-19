acb42bb8667d0c9273462f4f461218f48d0960ce
Bernhard Lutzmann <bernhard.lutzmann@gmail.com>
2021-02-19 20:29:53 +0100

    Lazy Loading der Wahlresultate

--- src/main/java/com/melonbase/microquark/repo/data/Wahlresultat.java
+++ src/main/java/com/melonbase/microquark/repo/data/Wahlresultat.java
@@ -1,17 +1,19 @@
 package com.melonbase.microquark.repo.data;
 
+import one.microstream.reference.Lazy;
+
 import java.util.List;
 import java.util.Map;
 
 public class Wahlresultat {
 
-  private Map<Kanton, List<Boolean>> stimmenByKanton;
+  private Lazy<Map<Kanton, List<Boolean>>> stimmenByKanton;
 
   public Wahlresultat(Map<Kanton, List<Boolean>> stimmenByKanton) {
-    this.stimmenByKanton = stimmenByKanton;
+    this.stimmenByKanton = Lazy.Reference(stimmenByKanton);
   }
 
   public Map<Kanton, List<Boolean>> getStimmenByKanton() {
-    return stimmenByKanton;
+    return Lazy.get(stimmenByKanton);
   }
 }
