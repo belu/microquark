137d327af33a3d257857e73c9c286240b779309a
Bernhard Lutzmann <bernhard.lutzmann@gmail.com>
2021-03-12 18:18:20 +0100

    Update to Kotlin 1.4.31, remove obsolete constructor for Jackson

--- src/main/kotlin/com/melonbase/microquark/rest/dto/inbound/NeueVolksabstimmung.kt
+++ src/main/kotlin/com/melonbase/microquark/rest/dto/inbound/NeueVolksabstimmung.kt
@@ -2,8 +2,4 @@ package com.melonbase.microquark.rest.dto.inbound
 
 import java.time.LocalDate
 
-data class NeueVolksabstimmung(val datum: LocalDate, val vorlagen: List<String>) {
-
-  // Jackson
-  constructor() : this(LocalDate.EPOCH, listOf())
-}
+data class NeueVolksabstimmung(val datum: LocalDate, val vorlagen: List<String>)
