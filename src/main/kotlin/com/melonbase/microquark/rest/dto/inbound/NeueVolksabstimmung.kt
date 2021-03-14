2af6bc8dc4e7991bf83ba52b5e6d78ceb9242a87
Bernhard Lutzmann <bernhard.lutzmann@gmail.com>
2021-03-14 13:48:41 +0100

    Add @RegisterForReflection to DTOs

--- src/main/kotlin/com/melonbase/microquark/rest/dto/inbound/NeueVolksabstimmung.kt
+++ src/main/kotlin/com/melonbase/microquark/rest/dto/inbound/NeueVolksabstimmung.kt
@@ -1,5 +1,7 @@
 package com.melonbase.microquark.rest.dto.inbound
 
+import io.quarkus.runtime.annotations.RegisterForReflection
 import java.time.LocalDate
 
+@RegisterForReflection
 data class NeueVolksabstimmung(val datum: LocalDate, val vorlagen: List<String>)
--- src/main/kotlin/com/melonbase/microquark/rest/dto/outbound/VolksabstimmungResultat.kt
+++ src/main/kotlin/com/melonbase/microquark/rest/dto/outbound/VolksabstimmungResultat.kt
@@ -1,5 +1,7 @@
 package com.melonbase.microquark.rest.dto.outbound
 
+import io.quarkus.runtime.annotations.RegisterForReflection
 import java.time.LocalDate
 
+@RegisterForReflection
 data class VolksabstimmungResultat(val datum: LocalDate, val vorlageResultate: List<VorlageResultat>)
